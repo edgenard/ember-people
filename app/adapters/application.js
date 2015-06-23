42d0a7f4593b9cfa4db9ce84ec271f56bbf53335
Emmanuel Genard <edgenard@gmail.com>
2015-06-23 15:07:23 -0400

    Fix url

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  host: "https://rails-people-api.herokuapp.com/",
+  host: "https://rails-people-api.herokuapp.com",
   ajax: function(url, method, hash) {
      hash = hash || {};
     hash.crossDomain = true;
