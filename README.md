25f32d5d23be0bb123dc964fd723f26ae05b2ba4
Emmanuel Genard <edgenard@gmail.com>
2015-06-23 14:41:50 -0400

    Initial gh pages

--- README.md
+++
@@ -1,6 +0,0 @@
-# Ember-people
-
-This is the repo for the ember app at [ember-people](https://emmanuelgenard.com/ember-people)
-
-
-
--- app/models/person.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  name: DS.attr("string"),
-  date_of_birth: DS.attr("string"),
-  deleted: DS.attr("boolean")
-});
-
